2502628d15c378828f85bede89f4f8b6392ae4b0
weihua19900704 <hua.wei@cern.ch>
2017-09-13 22:44:37 +0200

    add ngHB and HB collision check, aok

--- DevelopmentTools/HCALLMapChecker.cc
+++ DevelopmentTools/HCALLMapChecker.cc
@@ -1,5 +1,91 @@
 #include "HCALLMapChecker.h"
 
+bool HCALLMapChecker::checkHBLMapObject(std::vector<HBFrontEnd> myHBFrontEnd, std::vector<HBBackEnd> myHBBackEnd, std::vector<HBHPD> myHBHPD, std::vector<HBGeometry> myHBGeometry, std::vector<HBTriggerTower> myHBTriggerTower)
+{
+  bool passCollisionCheck = false;
+  bool passFrontEnd=true, passBackEnd=true, passGeometry=true;
+
+  std::vector<HBFrontEnd> copy_myHBFrontEnd(myHBFrontEnd.size());
+  std::copy( myHBFrontEnd.begin(), myHBFrontEnd.end(), copy_myHBFrontEnd.begin() );
+  std::vector<HBBackEnd> copy_myHBBackEnd(myHBBackEnd.size());
+  std::copy( myHBBackEnd.begin(), myHBBackEnd.end(), copy_myHBBackEnd.begin() );
+  std::vector<HBGeometry> copy_myHBGeometry(myHBGeometry.size());
+  std::copy( myHBGeometry.begin(), myHBGeometry.end(), copy_myHBGeometry.begin() );
+
+  for(std::vector<HBFrontEnd>::iterator i = copy_myHBFrontEnd.begin();i!= copy_myHBFrontEnd.end();i++)
+  {
+    for(std::vector<HBFrontEnd>::iterator j = i+1;j!= copy_myHBFrontEnd.end();j++)
+    {
+      bool foundCollision= ((*i).rbx==(*j).rbx) && ((*i).rm==(*j).rm) && ((*i).rm_fiber==(*j).rm_fiber) && ((*i).fiber_ch==(*j).fiber_ch);
+      if(foundCollision){ passFrontEnd=false; std::cout << "#RBX:" << (*j).rbx << ",RM:" << (*j).rm << ",RM_FI:" << (*j).rm_fiber << ",FI_CH" << (*j).fiber_ch << std::endl; }
+    }
+  }
+
+  for(std::vector<HBBackEnd>::iterator i = copy_myHBBackEnd.begin();i!= copy_myHBBackEnd.end();i++)
+  {
+    for(std::vector<HBBackEnd>::iterator j = i+1;j!= copy_myHBBackEnd.end();j++)
+    {
+      bool foundCollision= ((*i).ucrate==(*j).ucrate) && ((*i).uhtr==(*j).uhtr) && ((*i).uhtr_fiber==(*j).uhtr_fiber) && ((*i).fiber_ch==(*j).fiber_ch);
+      if(foundCollision){ passBackEnd=false; std::cout << "#Crate:" << (*j).ucrate << ",uHTR:" << (*j).uhtr << ",uHTR_FI:" << (*j).uhtr_fiber << ",FI_CH" << (*j).fiber_ch << std::endl; }
+    }
+  }
+
+  for(std::vector<HBGeometry>::iterator i = copy_myHBGeometry.begin();i!= copy_myHBGeometry.end();i++)
+  {
+    for(std::vector<HBGeometry>::iterator j = i+1;j!= copy_myHBGeometry.end();j++)
+    {
+      bool foundCollision= ((*i).side==(*j).side) && ((*i).eta==(*j).eta) && ((*i).phi==(*j).phi) && ((*i).depth==(*j).depth);
+      if(foundCollision){ passGeometry=false; std::cout << "#Side:" << (*j).side << ",Eta:" << (*j).eta << ",Phi:" << (*j).phi << ",Depth" << (*j).depth << std::endl; }
+    }
+  }
+
+  passCollisionCheck = passFrontEnd && passBackEnd && passGeometry;
+  return passCollisionCheck;
+}
+
+bool HCALLMapChecker::checkngHBLMapObject(std::vector<ngHBFrontEnd> myngHBFrontEnd, std::vector<ngHBBackEnd> myngHBBackEnd, std::vector<ngHBSiPM> myngHBSiPM, std::vector<ngHBGeometry> myngHBGeometry, std::vector<ngHBTriggerTower> myngHBTriggerTower)
+{
+  bool passCollisionCheck = false;
+  bool passFrontEnd=true, passBackEnd=true, passGeometry=true;
+
+  std::vector<ngHBFrontEnd> copy_myngHBFrontEnd(myngHBFrontEnd.size());
+  std::copy( myngHBFrontEnd.begin(), myngHBFrontEnd.end(), copy_myngHBFrontEnd.begin() );
+  std::vector<ngHBBackEnd> copy_myngHBBackEnd(myngHBBackEnd.size());
+  std::copy( myngHBBackEnd.begin(), myngHBBackEnd.end(), copy_myngHBBackEnd.begin() );
+  std::vector<ngHBGeometry> copy_myngHBGeometry(myngHBGeometry.size());
+  std::copy( myngHBGeometry.begin(), myngHBGeometry.end(), copy_myngHBGeometry.begin() );
+
+  for(std::vector<ngHBFrontEnd>::iterator i = copy_myngHBFrontEnd.begin();i!= copy_myngHBFrontEnd.end();i++)
+  {
+    for(std::vector<ngHBFrontEnd>::iterator j = i+1;j!= copy_myngHBFrontEnd.end();j++)
+    {
+      bool foundCollision= ((*i).rbx==(*j).rbx) && ((*i).rm==(*j).rm) && ((*i).rm_fiber==(*j).rm_fiber) && ((*i).fiber_ch==(*j).fiber_ch);
+      if(foundCollision){ passFrontEnd=false; std::cout << "#RBX:" << (*j).rbx << ",RM:" << (*j).rm << ",RM_FI:" << (*j).rm_fiber << ",FI_CH" << (*j).fiber_ch << std::endl; }
+    }
+  }
+
+  for(std::vector<ngHBBackEnd>::iterator i = copy_myngHBBackEnd.begin();i!= copy_myngHBBackEnd.end();i++)
+  {
+    for(std::vector<ngHBBackEnd>::iterator j = i+1;j!= copy_myngHBBackEnd.end();j++)
+    {
+      bool foundCollision= ((*i).ucrate==(*j).ucrate) && ((*i).uhtr==(*j).uhtr) && ((*i).uhtr_fiber==(*j).uhtr_fiber) && ((*i).fiber_ch==(*j).fiber_ch);
+      if(foundCollision){ passBackEnd=false; std::cout << "#Crate:" << (*j).ucrate << ",uHTR:" << (*j).uhtr << ",uHTR_FI:" << (*j).uhtr_fiber << ",FI_CH" << (*j).fiber_ch << std::endl; }
+    }
+  }
+
+  for(std::vector<ngHBGeometry>::iterator i = copy_myngHBGeometry.begin();i!= copy_myngHBGeometry.end();i++)
+  {
+    for(std::vector<ngHBGeometry>::iterator j = i+1;j!= copy_myngHBGeometry.end();j++)
+    {
+      bool foundCollision= ((*i).side==(*j).side) && ((*i).eta==(*j).eta) && ((*i).phi==(*j).phi) && ((*i).depth==(*j).depth);
+      if(foundCollision && (*j).subdet!="HBX"){ passGeometry=false; std::cout << "#Side:" << (*j).side << ",Eta:" << (*j).eta << ",Phi:" << (*j).phi << ",Depth" << (*j).depth << std::endl; }
+    }
+  }
+
+  passCollisionCheck = passFrontEnd && passBackEnd && passGeometry;
+  return passCollisionCheck;
+}
+
 bool HCALLMapChecker::checkngHELMapObject(std::vector<ngHEFrontEnd> myngHEFrontEnd, std::vector<ngHEBackEnd> myngHEBackEnd, std::vector<ngHESiPM> myngHESiPM, std::vector<ngHEGeometry> myngHEGeometry, std::vector<ngHETriggerTower> myngHETriggerTower)
 {
   bool passCollisionCheck = false;
