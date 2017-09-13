cb42dd41a7c2c749b835df2e9dcd411b9bcce2fa
weihua19900704 <hua.wei@cern.ch>
2017-09-13 08:11:59 +0200

    more on plotly??

--- DevelopmentTools/SQLQIEDBTest/lmap_db_test.py
+++ DevelopmentTools/SQLQIEDBTest/lmap_db_test.py
@@ -4,14 +4,31 @@ conn = sqlite3.connect('../officialMap/HCALLogicalMap.db')
 c = conn.cursor()
 print "Opened database successfully";
 
-cursor = c.execute("SELECT Side, Eta, RBX, RM, RM_FI, FI_CH from HBLogicalMap")
-for row in cursor:
-   print "Side = ", row[0]
-   print "Eta = ", row[1]
-   print "RBX = ", row[2]
-   print "RM = ", row[3]
-   print "RM_FI = ", row[4]
-   print "FI_CH = ", row[5], "\n"
+xlist = []
+ylist = []
+zlist = []
 
+rbx = ['HBP01','HBP02','HBP03','HBP04','HBP05','HBP06','HBP07','HBP08','HBP09','HBP10','HBP11','HBP12','HBP13','HBP14','HBP15','HBP16','HBP17','HBP18']
+#rbx = ['HBM01','HBM02','HBM03','HBM04','HBM05','HBM06','HBM07','HBM08','HBM09','HBM10','HBM11','HBM12','HBM13','HBM14','HBM15','HBM16','HBM17','HBM18']
+rm =[1,2,3,4]
+for thisrbx in rbx:
+  for thisrm in rm:
+    ylist.append(thisrbx+'RM'+str(thisrm))
+    cursor = c.execute("SELECT Side, Eta, ngRBX, RM, RM_FI, FI_CH from ngHBLogicalMap where ngRBX=? AND RM=? ORDER BY RM_FI,FI_CH",(thisrbx,thisrm))
+    zlistrbxrm=[]
+    for row in cursor:
+      if(len(ylist)==1):
+        xlist.append('RMFI'+str(row[4])+'FICH'+str(row[5]))
+      zlistrbxrm.append(row[0]*row[1])
+      #print "Side = ", row[0]
+      #print "Eta = ", row[1]
+      #print "RBX = ", row[2]
+      #print "RM = ", row[3]
+      #print "RM_FI = ", row[4]
+      #print "FI_CH = ", row[5], "\n"
+    zlist.append(zlistrbxrm)
+print xlist
+print ylist
+print zlist
 print "Operation done successfully";
 conn.close()
