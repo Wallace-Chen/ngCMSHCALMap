a961671f283dc449d439d814bb3f6d6dc2b012d6
weihua19900704 <hua.wei@cern.ch>
2018-01-23 08:19:34 +0100

    add CALIB_ tag in the det

--- DevelopmentTools/PandasTest/DumpEMapfromCalib.py
+++ DevelopmentTools/PandasTest/DumpEMapfromCalib.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
   print "#i crate slot tb dcc spigot htr_fi fib_ch det eta phi type"
   for index, row in df.iterrows():
     if row['t/b'] == 'top':
-      print row['index'], row['crate'], row['HTR'], 't', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+      print row['index'], row['crate'], row['HTR'], 't', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], 'CALIB_'+row['det'], row['eta'], row['phi'], row['type']
     elif row['t/b'] == 'bot':
-      print row['index'], row['crate'], row['HTR'], 'b', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+      print row['index'], row['crate'], row['HTR'], 'b', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], 'CALIB_'+row['det'], row['eta'], row['phi'], row['type']
     else:
-      print row['index'], row['crate'], row['HTR'], row['t/b'], row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+      print row['index'], row['crate'], row['HTR'], row['t/b'], row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], 'CALIB_'+row['det'], row['eta'], row['phi'], row['type']
