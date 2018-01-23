ffc72fc1018e118750432c94621ba0d5bd00ee3f
weihua19900704 <hua.wei@cern.ch>
2018-01-23 08:12:42 +0100

    fix calib channels

--- DevelopmentTools/PandasTest/DumpEMapfromCalib.py
+++ DevelopmentTools/PandasTest/DumpEMapfromCalib.py
@@ -8,4 +8,9 @@ if __name__ == '__main__':
 
   print "#i crate slot tb dcc spigot htr_fi fib_ch det eta phi type"
   for index, row in df.iterrows():
-    print row['index'], row['crate'], row['HTR'], row['t/b'], row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+    if row['t/b'] == 'top':
+      print row['index'], row['crate'], row['HTR'], 't', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+    elif row['t/b'] == 'bot':
+      print row['index'], row['crate'], row['HTR'], 'b', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
+    else:
+      print row['index'], row['crate'], row['HTR'], row['t/b'], row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], row['det'], row['eta'], row['phi'], row['type']
