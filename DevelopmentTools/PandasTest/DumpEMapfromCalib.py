95aa14906eb02dc44fa58942f20baf2a5bc2cec4
weihua19900704 <hua.wei@cern.ch>
2018-01-23 09:18:46 +0100

    update qies

--- DevelopmentTools/PandasTest/DumpEMapfromCalib.py
+++ DevelopmentTools/PandasTest/DumpEMapfromCalib.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
   fn = r'HCALmapCALIB_J.xls'
   df = pd.read_excel(fn, sheetname = 'uHTR')
   #print (df.head())
-
+  '''
   print "#i crate slot tb dcc spigot htr_fi fib_ch det eta phi type"
   for index, row in df.iterrows():
     if row['t/b'] == 'top':
@@ -14,3 +14,17 @@ if __name__ == '__main__':
       print row['index'], row['crate'], row['HTR'], 'b', row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], 'CALIB_'+row['det'], row['eta'], row['phi'], row['type']
     else:
       print row['index'], row['crate'], row['HTR'], row['t/b'], row['dcc'], row['spigot'], row['htr_fib'], row['fib_ch'], 'CALIB_'+row['det'], row['eta'], row['phi'], row['type']
+  '''
+  fn = r'ngCU_Lmap_template.xls'
+  df = pd.read_excel(fn, sheetname = 'c34fibers')
+  print "#HBHE CALIB channels Emap"
+  print "#i crate slot tb dcc spigot uhtr_fi fib_ch det eta phi depth"
+  for index, row in df.iterrows():
+      print row['i'], row['cr'], row['sl'], row['tb'], row['dcc'], row['spigot'], row['uhtr_fib'], row['fib_ch'], row['subdet'], row['ieta'], row['iphi'], row['depth']
+
+  '''
+  fn = r'HOHF_CUfiber_J_template.xls'
+  df = pd.read_excel(fn, sheetname = 'uHTR')
+  print "#HFHO CALIB channels Emap"
+  print "#i crate slot tb dcc spigot uhtr_fi fib_ch det eta phi depth"
+  '''
