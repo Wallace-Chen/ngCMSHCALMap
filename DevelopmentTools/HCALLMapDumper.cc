a45405ee0089febeacb58072ae81a602ee1db45a
Yuan <54chenyuan@gmail.com>
2018-02-23 05:50:56 +0100

    Change cp to pp in output

--- DevelopmentTools/HCALLMapDumper.cc
+++ DevelopmentTools/HCALLMapDumper.cc
@@ -1245,7 +1245,7 @@ void HCALLMapDumper::printHOCalibLMapObject(std::vector<HOCalib> myHOCalib)
 {
   std::cout << "#Dumping HO Calib LMap Object..." << std::endl;
   std::cout << "#"
-            << std::setw(6) << "side" << std::setw(6) << "eta" << std::setw(6) << "phi" << std::setw(6) << "dphi" << std::setw(6) << "depth" << std::setw(6) << "rbx" << std::setw(9) << "sector" << std::setw(6) << "rm" << std::setw(6) << "rm_fi" << std::setw(6) << "rm_ch" << std::setw(6) << "det" << std::setw(6) << "cpCol" << std::setw(6) << "cpRow" << std::setw(6) << "cpLC" << std::setw(6) << "crate" << std::setw(6) << "HTR" << std::setw(6) << "fpga" << std::setw(8) << "htr_fi"
+            << std::setw(6) << "side" << std::setw(6) << "eta" << std::setw(6) << "phi" << std::setw(6) << "dphi" << std::setw(6) << "depth" << std::setw(6) << "rbx" << std::setw(9) << "sector" << std::setw(6) << "rm" << std::setw(6) << "rm_fi" << std::setw(6) << "rm_ch" << std::setw(6) << "det" << std::setw(6) << "ppCol" << std::setw(6) << "ppRow" << std::setw(6) << "ppLC" << std::setw(6) << "crate" << std::setw(6) << "HTR" << std::setw(6) << "fpga" << std::setw(8) << "htr_fi"
             <<std::endl;
 
   for(auto i=0;i<myHOCalib.size();i++)
