8996c367f149ad94d8a0a571d91ab98a3258d755
weihua19900704 <hua.wei@cern.ch>
2018-03-01 00:35:55 +0100

    good, we add lasermon

--- DevelopmentTools/dumpEMap.sh
+++ DevelopmentTools/dumpEMap.sh
@@ -12,3 +12,6 @@
 echo "#Dumping HOCalib EMap Object..."
 echo "#         i    cr    sl    tb   dcc  spigot fib/slb fibch/slbch   subdet   eta   phi   dep"
 cat officialMap/HO_CU_emap_2018_K.txt | grep "CALIB_HO"
+echo "#Dumping LaserCalib EMap Object..."
+echo "#         i    cr    sl    tb   dcc  spigot fib/slb fibch/slbch   subdet   eta   phi   dep"
+cat officialMap/2018HCALEMap_lasermonCalib_K_20180227.txt | grep "LASMON"
