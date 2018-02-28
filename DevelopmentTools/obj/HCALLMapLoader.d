cd5e742d412ffde6874ec543b5092459add4fe73
weihua19900704 <hua.wei@cern.ch>
2018-03-01 00:40:39 +0100

    good

---
+++ DevelopmentTools/obj/HCALLMapLoader.d
@@ -0,0 +1,20 @@
+obj/HCALLMapLoader.o: HCALLMapLoader.cc HCALLMapLoader.h \
+ HBMappingObject.h HEMappingObject.h ngHBMappingObject.h \
+ ngHEMappingObject.h HFMappingObject.h ngHFMappingObject.h \
+ HOMappingObject.h
+
+HCALLMapLoader.h:
+
+HBMappingObject.h:
+
+HEMappingObject.h:
+
+ngHBMappingObject.h:
+
+ngHEMappingObject.h:
+
+HFMappingObject.h:
+
+ngHFMappingObject.h:
+
+HOMappingObject.h:
---
+++ DevelopmentTools/obj/HCALTPProducer.d
@@ -0,0 +1,20 @@
+obj/HCALTPProducer.o: HCALTPProducer.cc HCALTPProducer.h \
+ HBMappingObject.h HEMappingObject.h ngHBMappingObject.h \
+ ngHEMappingObject.h HFMappingObject.h ngHFMappingObject.h \
+ HOMappingObject.h
+
+HCALTPProducer.h:
+
+HBMappingObject.h:
+
+HEMappingObject.h:
+
+ngHBMappingObject.h:
+
+ngHEMappingObject.h:
+
+HFMappingObject.h:
+
+ngHFMappingObject.h:
+
+HOMappingObject.h:
